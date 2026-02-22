47e68a4da560ada76855ec657d96ad90f682bf50
elaatanjung7-jpg <elaatanjung7@gmail.com>
2026-02-22 11:28:52 +0700

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,1 +1,32 @@
+function register(){
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
+    if(!name || !email || !password){
+        alert("Harap isi semua data!");
+        return;
+    }
+
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+
+    alert("Pendaftaran berhasil!");
+}
+
+function login(){
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if(email === localStorage.getItem("email") &&
+       password === localStorage.getItem("password")){
+        alert("Login berhasil!");
+        window.location.href = "index.html";
+    } else {
+        alert("Email atau Password salah!");
+    }
+}
+
+function kirimPesan(){
+    alert("Pesan berhasil dikirim!");
+}
